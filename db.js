ec5fceb8bf6dd681ab23a529505b83d50429b91a
Araujomann <lucastheeyepatch@gmail.com>
2025-01-18 22:03:34 -0300

    changing environment variable database url

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASE_URL);
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("mongoDB conectado com sucesso!");
   } catch (error) {
     console.log("Erro ao conectar com o mongoDB:", error.message);
