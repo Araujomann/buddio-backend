a396d43a46ad2a1d621b9ce7d68c3682c22252e8
Araujomann <lucastheeyepatch@gmail.com>
2025-01-22 22:46:32 -0300

    try fix::

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ const io = new Server(server, {
             process.env.FRONTEND_URL,
             "http://localhost:5173",
             "http://localhost:5174",
-            "http://localhost:5000 ",
+            "http://localhost:5000",
         ],
         methods: ["GET", "POST"],
     },
